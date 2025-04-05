0ec2fbc4533b9344f760ef4103cf3a72f2ef484e
Origami_SL <shenli@buaa.edu.cn>
2025-04-05 22:20:43 +0800

    Update creat_symbolic_link.sh

--- creat_symbolic_link.sh
+++ creat_symbolic_link.sh
@@ -1,5 +1,5 @@
-your_dataset_root='/home/lhg/work/ssd_new/AVSBench_all/COMBO-AVS-main/AVS_dataset'
-your_pretrained_path='/home/lhg/work/ssd_new/AVSBench_all/COMBO-AVS-main/pretrained'
+your_dataset_root='./AVS_dataset'
+your_pretrained_path='./pretrained'
 
 ln -s "$your_dataset_root" ./NTAVS_R50
 
